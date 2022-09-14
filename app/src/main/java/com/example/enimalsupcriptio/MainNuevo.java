eef899b9d08d6e5a9ae9c6574c1094a97df1b0bf
SergioAinol <samtom24680@gmail.com>
2022-09-14 10:53:12 -0300

    Activitys casi finalizados por Gui

--- app/src/main/java/com/example/enimalsupcriptio/MainNuevo.java
+++ app/src/main/java/com/example/enimalsupcriptio/MainNuevo.java
@@ -10,22 +10,19 @@ import android.widget.Toast;
 
 public class MainNuevo extends AppCompatActivity implements View.OnClickListener{
 
-Button btnNuevoSalir;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_nuevo);
 
-        btnNuevoSalir=findViewById(R.id.btnNuevoSalir);
 
     }
 
 
     @Override
     public void onClick(View view) {
-        Toast.makeText(getApplicationContext(),"Cambiando a Registro",Toast.LENGTH_LONG).show();
-        Intent i = new Intent(MainNuevo.this,MainActivity.class);
-        startActivity(i);
+
     }
 }
